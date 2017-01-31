56f55d23f07209c1bec395c980c263aa970d36b3
Mike Perry <mike@astronomer.io>
2017-01-31 16:39:39 -0500

    schema functions are expect a mongoose model function to compile the models on a given connection

--- test/index.js
+++ test/index.js
@@ -2,18 +2,19 @@ const assert = require('chai').assert;
 const Mongoose = require('mongoose').Mongoose;
 const mockgoose = require('mockgoose');
 const mongoose = new Mongoose();
-const models = require('../lib/')(mongoose);
+const createModels = require('../lib/');
 
 describe('buildSchema', function () {
     before(function (done) {
         mockgoose(mongoose).then(function () {
             mongoose.connect('mongodb://mongo/test');
             mongoose.connection.once('open', done);
-            mongoose.connection.once("error", done);
+            mongoose.connection.once('error', done);
         });
     });
 
     it('creates models on given connection', function () {
+        const models = createModels(mongoose.connection.model.bind(mongoose.connection));
         Object.keys(models).forEach(key => {
             assert(models[key].db === mongoose.connection, 'Models should be created on the given mongoose connection');
         });
